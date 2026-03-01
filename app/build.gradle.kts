587d6d167f5e3924cbc73a9bc6c2a49b8d678708
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2026-03-01 15:54:05 +0200

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "fi.pomeranssi.bookline"
         minSdk = 28
         targetSdk = 36
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
